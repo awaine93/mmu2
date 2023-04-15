3192ac2a4337269683bf1826dbe918cd1f36c9dc
baine101 <asikbaine@gmail.com>
2023-04-15 02:48:59 +0100

    moved switch to command handler

--- mmu2-diy/application.cpp
+++ mmu2-diy/application.cpp
@@ -181,7 +181,6 @@ void Application::disableAllMotors(){
 
 // Handles any command incomming from the printer's serial
 void Application::checkSerialInterface() {
-	unsigned char c1, c2, c3;
 	int cnt;
 	String inputLine;
 	int findaStatus;
@@ -197,163 +196,8 @@ void Application::checkSerialInterface() {
 		}
 process_more_commands:  // parse the inbound command
 
-		c1 = inputLine[index++];  
-		c2 = inputLine[index++]; 
-		c3 = inputLine[index++];
-		
-		// process commands coming from the mk3 controller : Extract this to a method so that 
-		// both USB and Serial printer can run the commands
-		//***********************************************************************************
-		// Commands still to be implemented: F0 (Filament type select),
-		// E0->E4 (Eject Filament), R0 (recover from eject)
-		//***********************************************************************************
-		switch (c1) {
-		case 'X':
-			// RESET COMMAND
-			Serial.println("X: Resetting MMU");
-			break;
-		case 'T':
-			Serial.println("T: Tool change");
-		
-			
-			time4 = millis();           // grab the current time
-			
-			// request for idler and selector based on filament number
-			if ((c2 >= '0')  && (c2 <= '4')) {
-				toolChange(c2);
-			} else {
-				Serial.println(F("T: Invalid filament Selection"));
-			}
+		index = commandHandler.handlePrinterCommand(inputLine, index);
 
-			Serial1.print(F("ok\n"));              // send command acknowledge back to mk3 controller
-			time5 = millis();          // grab the current time
-
-			break;
-		case 'C':
-			Serial.println(F("C: Moving filament to extruder"));
-			// move filament from selector ALL the way to printhead
-			
-			// filamentLoadToMK3();
-			filamentController.filamentLoadWithBondTechGear();
-			// delay(200);
-			Serial1.print(F("ok\n"));
-			break;
-
-		case 'U':
-			// UNLOAD FILIMENT COMMAND
-			// request for filament unload
-
-			Serial.println(F("U: Filament Unload Selected"));
-			//*******************************************************************************************************
-			//*  FIX:  don't go all the way to the end ... be smarter
-			//******************************************************************************************************
-			//* unparking is more elegant 10.12.18
-			if (idlerController.status == QUICKPARKED) {
-				idlerController.quickunParkIdler();             // un-park the idler from a quick park
-			}
-			if (idlerController.status == INACTIVE) {
-				idlerController.unParkIdler();                    // turn on the idler motor
-			}
-
-			if ((c2 >= '0') && (c2 <= '4')) {
-
-				filamentController.unloadFilamentToFinda();
-				idlerController.parkIdler();
-				Serial.println(F("U: Sending Filament Unload Acknowledge to MK3"));
-				delay(200);
-				Serial1.print(F("ok\n"));
-
-			} else {
-				Serial.println(F("U: Invalid filament Unload Requested"));
-				delay(200);
-				Serial1.print(F("ok\n"));
-			}
-			break;
-		case 'L':
-			Serial.println(F("L: Filament Load Selected"));
-
-			if (idlerController.status == QUICKPARKED) {
-				idlerController.quickunParkIdler();             // un-park the idler from a quick park
-			}
-			if (idlerController.status == INACTIVE) {
-				idlerController.unParkIdler();                    // turn on the idler motor
-			}
-
-			if (colorSelector.status == INACTIVE)
-				colorSelector.activate();         // turn on the color selector motor
-
-			if ((c2 >= '0') && (c2 <= '4')) {
-				Serial.println(F("L: Moving the bearing idler"));
-				idlerController.idlerSelector((int)c2);   // move the filament selector stepper motor to the right spot
-				Serial.println(F("L: Moving the color selector"));
-				colorSelector.select(c2);     // move the color Selector stepper Motor to the right spot
-				Serial.println(F("L: Loading the Filament"));
-				// filamentController.loadFilament(CCW);
-				filamentController.loadFilamentToFinda();
-				idlerController.parkIdler();             // turn off the idler roller
-
-				Serial.println(F("L: Sending Filament Load Acknowledge to MK3"));
-
-				delay(200);
-
-				Serial1.print(F("ok\n"));
-
-
-
-			} else {
-				Serial.println(F("Error: Invalid Filament Number Selected"));
-			}
-			break;
-
-		case 'S':
-			// request for firmware version
-			switch (c2) {
-			case '0':
-				Serial.println(F("S: Sending back OK to MK3"));
-				Serial1.print(F("ok\n"));
-				break;
-			case '1':
-				Serial.println(F("S: FW Version Request"));
-				Serial1.print(FW_VERSION);
-				Serial1.print(F("ok\n"));
-				break;
-			case '2':
-				Serial.println(F("S: Build Number Request"));
-				Serial.println(F("Initial Communication with MK3 Controller: Successful"));
-				Serial1.print(FW_BUILDNR);
-				Serial1.print(F("ok\n"));
-				break;
-			default:
-				Serial.println(F("S: Unable to process S Command"));
-				break;
-			}
-			break;
-		case 'P':
-
-			// check FINDA status
-			// Serial.println(F("Check FINDA Status Request"));
-			findaStatus = filamentController.isFilamentLoaded();
-			if (findaStatus == 1) {
-				Serial1.println(F("1"));
-			}
-			else {
-				Serial1.println(F("0"));
-			}
-			Serial1.print(F("ok\n"));
-
-			break;
-		case 'F':  // 'F' command is acknowledged but no processing goes on at the moment
-			// will be useful for flexible material down the road
-			Serial.println(F("Filament Type Selected: "));
-			Serial.println(c2);
-			Serial1.print(F("ok\n"));                        // send back OK to the mk3
-			break;
-		default:
-			Serial.print(F("ERROR: unrecognized command from the MK3 controller"));
-			Serial1.print(F("ok\n"));
-
-
-		}  // end of switch statement
 #ifdef NOTDEF
 		if (cnt != 3) {
 
@@ -513,7 +357,7 @@ void Application::toolChange( char selection) {
 	//***********************************************************************************************
 	if (newExtruder == filamentSelection) {  // already at the correct filament selection
 		
-		if (digitalRead(findaPin) == 1) {            // no filament loaded
+		if (filamentController.isFilamentLoaded() == 0) {            // no filament loaded
 			Serial.println(F("toolChange: filament not currently loaded, loading ..."));
 
 			idlerController.idlerSelector((int)selection);   // move the filament selector stepper motor to the right spot
